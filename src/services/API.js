22f35342f4da8eb708d7313b09571ff1c3bff9c1
Marco <marcoantonio99d@gmail.com>
2022-07-28 13:15:30 -0300

    endereço

--- src/services/API.js
+++ src/services/API.js
@@ -1,29 +1,29 @@
 export const getAll = async () => {
-  const response = await fetch('https://vehicles-challenge-backend.herokuapp.com/ads');
+  const response = await fetch('https://vehicles-challenge.herokuapp.com/ads');
   const data = await response.json();
   return data;
 };
 
 export const getById = async (id) => {
-  const response = await fetch(`https://vehicles-challenge-backend.herokuapp.com/ads/${id}`);
+  const response = await fetch(`https://vehicles-challenge.herokuapp.com/ads/${id}`);
   const data = await response.json();
   return data;
 };
 
 export const getByQuery = async (query) => {
-  const response = await fetch(`https://vehicles-challenge-backend.herokuapp.com/ads/search?q=${query}`);
+  const response = await fetch(`https://vehicles-challenge.herokuapp.com/ads/search?q=${query}`);
   const data = await response.json();
   return data;
 }
 
 export const getByFilters = async (b, c, y, min, max) => {
-  const response = await fetch(`https://vehicles-challenge-backend.herokuapp.com/ads/filters?b=${b}&c=${c}&y=${y}&min=${min}&max=${max}`);
+  const response = await fetch(`https://vehicles-challenge.herokuapp.com/ads/filters?b=${b}&c=${c}&y=${y}&min=${min}&max=${max}`);
   const data = await response.json();
   return data;
 }
 
 export const createNewVehicle = async (vehicle) => {
-  const response = await fetch('https://vehicles-challenge-backend.herokuapp.com/ads', {
+  const response = await fetch('https://vehicles-challenge.herokuapp.com/ads', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -35,7 +35,7 @@ export const createNewVehicle = async (vehicle) => {
 }
 
 export const deleteVehicle = async (id) => {
-  const response = await fetch(`https://vehicles-challenge-backend.herokuapp.com/ads/${id}`, {
+  const response = await fetch(`https://vehicles-challenge.herokuapp.com/ads/${id}`, {
     method: 'DELETE',
   });
   const data = await response.json();
@@ -43,7 +43,7 @@ export const deleteVehicle = async (id) => {
 };
 
 export const updateVehicle = async (vehicle, id) => {
-  const response = await fetch(`https://vehicles-challenge-backend.herokuapp.com/ads/${id}`, {
+  const response = await fetch(`https://vehicles-challenge.herokuapp.com/ads/${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
@@ -55,7 +55,7 @@ export const updateVehicle = async (vehicle, id) => {
 };
 
 export const toggleFavorite = async (favorite, id) => {
-  const response = await fetch(`https://vehicles-challenge-backend.herokuapp.com/ads/favorite/${id}`, {
+  const response = await fetch(`https://vehicles-challenge.herokuapp.com/ads/favorite/${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
